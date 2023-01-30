31df7c969ec0d026bff2399518281bd3f862f931
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-01-30 20:35:54 +0530

    patern printing
    Write a program to patern printing

---
+++ Python/patern_printing.py
@@ -0,0 +1,163 @@
+n=int(input("enter number of rows"))
+i=1
+while(n>0):
+      b=1
+      while(b<i):
+        print(" ",end=" ")
+        b=b+1
+j=1
+while(j<=(n*2)-i):
+    print('*',end=' ')
+    j=j+1            
+print()
+n=n-1
+i=i+1
+
+
+
+
+"""
+k=1
+i=1
+while(i<=5):
+      b=1
+      while(b<=5-i):
+        print(" ",end=" ")
+        b=b+1
+      j=1
+      while(j<=k):
+        print(j,end=' ')
+        j=j+1            
+k=k+2        
+print()
+i=i+1
+"""
+
+
+
+
+
+
+
+""" 
+        1 
+      2 2
+    3 3 3
+  4 4 4 4
+5 5 5 5 5
+
+i=1
+while(i<=5):
+    b=1
+    while(b<=5-i):
+        print(" ",end=" ")
+        b=b+1
+
+    j=1
+    while(j<=i):
+        print(i,end=' ')
+        j=j+1
+    print()
+    i=i+1 
+   """
+   
+"""   
+        1 
+      1 2
+    1 2 3
+  1 2 3 4
+1 2 3 4 5
+
+i=1
+while(i<=5):
+    b=1
+    while(b<=5-i):
+        print(" ",end=" ")
+        b=b+1
+
+    j=1
+    while(j<=i):
+        print(j,end=' ')
+        j=j+1
+    print()
+    i=i+1
+
+"""
+
+"""
+        * 
+      * *
+    * * *
+  * * * *
+* * * * *
+
+
+i=1
+while(i<=5):
+    b=1
+    while(b<=5-i):
+        print(" ",end=" ")
+        b=b+1
+
+    j=1
+    while(j<=i):
+        print('*',end=' ')
+        j=j+1
+    print()
+    i=i+1
+    """
+
+"""
+* 
+* *
+* * * 
+* * * * 
+* * * * * 
+i=1
+while(i<=5):
+    j=1
+    while(j<=i):
+        print('*',end=' ')
+        j=j+1
+    print()
+    i=i+1
+"""
+
+"""
+1 
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+
+
+
+i=1
+while(i<=5):
+    j=1
+    while(j<=i):
+        print('*',end=' ')
+        j=j+1
+    print()
+    i=i+1
+"""
+"""
+1 
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+
+
+
+i=1
+while(i<=5):
+    j=1
+    while(j<=i):
+        print(j,end=' ')
+        j=j+1
+    print()
+    i=i+1
+
+    """
