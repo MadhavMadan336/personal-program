f7a12c0b679b329083886f1b0b2050f21b61eb6b
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-01-31 16:55:21 +0530

    Write a program to print max of two
    Write a program to print max of two

---
+++ Python/max_of_two.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# printing the maximum value
+if(num1 > num2):
+  print(num1, "is greater")
+elif(num1 < num2):
+    print(num2, "is greater")
+else:
+    print("Both are equal")
