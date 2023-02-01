e04fa7774f589f0f02fe1992dbce0339a4607d77
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-02-01 16:18:09 +0530

    find max between three number
    Write a program find max between three number

---
+++ Python/find_max_between_three_number.py
@@ -0,0 +1,9 @@
+a= int(input("Enter the first number:  "))
+b= int(input("Enter the second number: "))
+c= int(input("Enter the third number:  "))
+if (a>b) and (a>c):
+    print(" First is the largest number=",a)
+elif (b>a) and (b>c):
+    print(" Second is the largest number=",b)  
+else:
+    print("Third is the largest number=",c)    
