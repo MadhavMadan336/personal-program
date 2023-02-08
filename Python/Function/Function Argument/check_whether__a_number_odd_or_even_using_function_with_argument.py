4fdbed1c28b76fcf7f1e0566bf1df6880343cca0
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-02-08 17:16:49 +0530

    Create check_whether__a_number_odd_or_even_using_function_with_argument.py

---
+++ Python/Function/Function Argument/check_whether__a_number_odd_or_even_using_function_with_argument.py
@@ -0,0 +1,9 @@
+def oddeve(a):
+    if (a%2==0):
+        print("the number is even")
+    else:
+        print("the number is odd")   
+
+b=int(input("enter the number to check::::"))
+oddeve(b)
+ 
