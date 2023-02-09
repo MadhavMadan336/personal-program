02a5142c392dd905f4f2083e6cc76b33ab0109b3
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-02-09 19:23:47 +0530

    Create switch fall through statement.c

---
+++ c/switch fall through statement.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("\n enter the number");
+    scanf("%d",&a);
+    switch(a)
+    {
+        case 1:
+        printf("\n January");
+        
+        case 2:
+        printf("\n February");
+        
+        case 3:
+        printf("\n March");
+        
+        case 4:
+        printf("\n April");
+        
+        case 5:
+        printf("\n May");
+        
+        case 6:
+        printf("\n June");
+  
+        case 7:
+        printf("\n July");
+       
+        case 8:
+        printf("\n August");
+        
+        case 9:
+        printf("\n September");
+        
+        case 10:
+        printf("\n October");
+        
+        case 11:
+        printf("\n November");
+        
+        case 12:
+        printf("\n December");
+        
+   
+        default:
+        printf("\n wrong input");
+    }
+    return '0';
+}
