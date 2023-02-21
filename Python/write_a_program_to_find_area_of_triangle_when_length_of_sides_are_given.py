ec9a07fe15da2119df59aeea69920d3bec113aa7
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-02-21 10:15:46 +0530

    Update write_a_program_to_find_area_of_triangle_when_length_of_sides_are_given.py

--- Python/write_a_program_to_find_area_of_triangle_when_length_of_sides_are_given.py
+++ Python/write_a_program_to_find_area_of_triangle_when_length_of_sides_are_given.py
@@ -1,6 +1,6 @@
 a=5
 b=8
 c=6
-s=(a+b+c)/2
-AREA=(s*(s-a)*(s-b)*(s-c))**0.5
-print("The area of the trangle is:"AREA)
+s = (a + b + c) / 2 
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5  
+print('The area of the triangle is %0.2f' %area)
