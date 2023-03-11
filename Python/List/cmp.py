78cfde40419d995bd13073138ffb2ea249827413
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-03-11 08:58:53 +0530

    Create cmp.py

---
+++ Python/List/cmp.py
@@ -0,0 +1,10 @@
+a=[2,4,6,8]
+b=[2,4,6,9]
+c=[2,4,6,9,11]
+d=[2,4,6,8]
+print("comparing a and b:")
+print(cmp(a,b))
+print("comparing b and c:")
+print(cmp(b,c))
+print("comparing a and d:")
+print(cmp(a,d))
