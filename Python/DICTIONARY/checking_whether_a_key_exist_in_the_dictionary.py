dcf4f41b4b53cea371a1ba0813e36f35e9bd7371
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-04-23 00:48:53 +0530

    Create checking_whether_a_key_exist_in_the_dictionary.py

---
+++ Python/DICTIONARY/checking_whether_a_key_exist_in_the_dictionary.py
@@ -0,0 +1,5 @@
+dict1={"brand":"Suzuki","model":"Dzire","year":2020}
+if "model"in dict1:
+    print("yes,'model'is one of  the keys in the dictionary")
+else:
+    print("no,'model'is not one of the key in the dictionary")
