494a14a264d6da01a0986a3a1a832ba76055ed2a
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-05-15 20:40:04 +0530

    Create indexing_in_1D_array.py

---
+++ Python/DICTIONARY/indexing_in_1D_array.py
@@ -0,0 +1,19 @@
+import numpy as np
+a=[]
+size= int (input("How many Numbers?"))
+for i in range(size):
+  val=int(input("Enter Number"))
+  a.append(val)
+arr=np.array(a)
+for i in range(arr.size):
+  print(arr[i])
+
+
+
+
+
+
+
+
+
+
