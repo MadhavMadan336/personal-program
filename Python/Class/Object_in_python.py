e7e5c534fc8674ee23c5be99fdc7490f28f226c4
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-06-18 09:08:47 +0530

    Create Object_in_python.py

---
+++ Python/Class/Object_in_python.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+    
+    def start_engine(self):
+        print("Engine started.")
+    
+    def stop_engine(self):
+        print("Engine stopped.")
