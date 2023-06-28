d7bfbe02d220facfe386ec4b4011ca8a612c176a
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-06-28 07:18:47 +0530

    Create 4_line.py

---
+++ Python/Class/simple program/4_line.py
@@ -0,0 +1,12 @@
+# 1. Print a greeting
+print("Hello, World!")
+
+# 2. Get user input
+name = input("What is your name? ")
+
+# 3. Print a personalized message
+print("Nice to meet you, " + name + "!")
+
+# 4. Perform a mathematical calculation
+result = 3 + 5
+print("The result is:", result)
