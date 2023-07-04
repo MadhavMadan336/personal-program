77f668e053b2439ff3d05baf9228aed7047c0e02
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-07-04 09:06:34 +0530

    Create class_p_p.py

---
+++ Python/Class/class_p_p.py
@@ -0,0 +1,12 @@
+class PythonProgram:
+    # Class attributes and methods go here
+    pass
+class PythonProgram:
+    version = '1.0'
+
+    def greet(self):
+        print(f"Welcome to Python Program version {self.version}!")
+
+# Creating an instance of the class
+program = PythonProgram()
+program.greet()
