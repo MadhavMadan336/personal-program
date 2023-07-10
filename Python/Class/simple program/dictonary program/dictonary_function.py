432128798b20485ff877373b9ce87706ea745905
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-07-10 07:56:12 +0530

    Create dictonary_function.py

---
+++ Python/Class/simple program/dictonary program/dictonary_function.py
@@ -0,0 +1,14 @@
+# Creating a dictionary using the dict() function
+my_dict = dict()
+# or
+my_dict = {}
+
+# Adding key-value pairs to the dictionary
+my_dict['name'] = 'John'
+my_dict['age'] = 30
+my_dict['city'] = 'New York'
+
+# Accessing values from the dictionary
+print(my_dict['name'])  # Output: John
+print(my_dict['age'])   # Output: 30
+print(my_dict['city'])  # Output: New York
