5f252ac5f8e666f2ea08022364c977a5582b3a6a
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-07-19 08:38:15 +0530

    Create List_simple_program.py

---
+++ Python/Class/simple program/List/List_simple_program.py
@@ -0,0 +1,12 @@
+# Variable assignment
+x = 10
+y = 5
+
+# Basic arithmetic operations
+sum_result = x + y
+sub_result = x - y
+mul_result = x * y
+div_result = x / y
+mod_result = x % y
+
+print(sum_result, sub_result, mul_result, div_result, mod_result)
