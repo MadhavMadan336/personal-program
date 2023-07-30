aa548be45d39323e92307c5f580de702bec95f60
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-07-30 09:40:26 +0530

    Create tuple_dictionary_list_array.py

---
+++ Python/Class/simple program/Tuple/tuple_dictionary_list_array.py
@@ -0,0 +1,16 @@
+# Tuple example
+tuple_example = (1, 2, 3, 4, 5)
+print("Tuple:", tuple_example)
+
+# Dictionary example
+dictionary_example = {'apple': 1, 'banana': 2, 'orange': 3}
+print("Dictionary:", dictionary_example)
+
+# List example
+list_example = [10, 20, 30, 40, 50]
+print("List:", list_example)
+
+# Array example (using NumPy library)
+import numpy as np
+array_example = np.array([100, 200, 300, 400, 500])
+print("Array:", array_example)
