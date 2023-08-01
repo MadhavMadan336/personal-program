ee23bdb3267bae008897b115da88872e38cea95b
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-08-01 09:11:31 +0530

    Create list_dictionary_tuple_array.py

---
+++ Python/Class/simple program/dictonary program/list_dictionary_tuple_array.py
@@ -0,0 +1,21 @@
+# Dictionary example
+student_grades = {
+    'Alice': 85,
+    'Bob': 72,
+    'Charlie': 90,
+}
+
+# Tuple example
+coordinates = (10, 20)
+
+# List example
+fruits = ['apple', 'banana', 'orange']
+
+# Array example (using NumPy library)
+import numpy as np
+data_array = np.array([1, 2, 3, 4, 5])
+
+print("Student grades:", student_grades)
+print("Coordinates:", coordinates)
+print("Fruits:", fruits)
+print("Data array:", data_array)
