69787a6f6a64b61b9e3f497eb0bab99dace962c4
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-08-23 10:08:21 +0530

    Create Dic_arr_tup.py

---
+++ Python/Class/simple program/dictonary program/Dic_arr_tup.py
@@ -0,0 +1,21 @@
+# Dictionary containing information about a person
+person = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+
+# Tuple representing coordinates
+coordinates = (10, 20)
+
+# List of favorite colors
+colors = ["blue", "green", "red"]
+
+# Array of numbers using NumPy (you'll need to install the numpy package)
+import numpy as np
+numbers_array = np.array([1, 2, 3, 4, 5])
+
+print("Person:", person)
+print("Coordinates:", coordinates)
+print("Favorite Colors:", colors)
+print("Numbers Array:", numbers_array)
