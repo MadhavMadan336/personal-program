60262a29976b0aac4d57f1d85787fb050f1fcbc4
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-08-29 11:15:11 +0530

    Create array_tuple.py

---
+++ Python/Class/simple program/Tuple/array_tuple.py
@@ -0,0 +1,10 @@
+# Create an array of tuples
+array_of_tuples = [(1, 'apple'), (2, 'banana'), (3, 'orange')]
+
+# Accessing elements
+print(array_of_tuples[0])  # Output: (1, 'apple')
+print(array_of_tuples[1])  # Output: (2, 'banana')
+
+# Accessing elements within a tuple
+print(array_of_tuples[0][0])  # Output: 1
+print(array_of_tuples[0][1])  # Output: 'apple'
