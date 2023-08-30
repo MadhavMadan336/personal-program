537a81d7ed327196ffaedd4e86160608908c6c21
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-08-30 10:32:36 +0530

    Create array_dictionary.py

---
+++ Python/Class/simple program/dictonary program/array_dictionary.py
@@ -0,0 +1,25 @@
+# Creating an array of numbers
+numbers_array = [1, 2, 3, 4, 5]
+
+# Creating a dictionary of names and ages
+ages_dict = {
+    "Alice": 25,
+    "Bob": 30,
+    "Charlie": 28,
+    "David": 22,
+    "Eve": 26
+}
+
+# Accessing elements from the array
+print("Second element in the array:", numbers_array[1])
+
+# Accessing values from the dictionary
+print("Age of Bob:", ages_dict["Bob"])
+
+# Modifying elements in the array
+numbers_array[2] = 6
+print("Updated array:", numbers_array)
+
+# Modifying values in the dictionary
+ages_dict["Alice"] = 26
+print("Updated age of Alice:", ages_dict["Alice"])
