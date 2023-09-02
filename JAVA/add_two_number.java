56884b40df04e1a9d747d1a6b2fc55476fc20f93
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-09-02 10:24:00 +0530

    Create add_two_number.java

---
+++ JAVA/add_two_number.java
@@ -0,0 +1,10 @@
+public class AddTwoNumbers {
+    public static void main(String[] args){//main function//
+    
+    //declaring values//
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;//addition of 2 numbers//
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
