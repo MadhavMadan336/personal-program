a2446572c9c6a5975f9b1739f4be692c7094ccfb
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-09-10 16:34:02 +0530

    Create division_of_two_numbers.java

---
+++ JAVA/division_of_two_numbers.java
@@ -0,0 +1,13 @@
+
+
+public class division_of_two_numbers {
+
+	public static void main(String[] args) {//main function//
+		int a=100;
+		int b=20;
+		int c=a/b;
+		System.out.println("The number a="+a+ " \nThe number b=" +b+" \nDivision="+c);
+
+	}
+
+}
