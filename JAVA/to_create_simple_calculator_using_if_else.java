50330e01c179e6b6cf164b6d5d670b6c80f0a6bb
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-09-28 18:40:12 +0530

    Create to_create_simple_calculator_using_if_else.java

---
+++ JAVA/to_create_simple_calculator_using_if_else.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class to_create_simple_calculator_using_if_else {
+    public static void main(String[] args) {
+        int a, b, ch; // Declare integer variables a, b, and sum
+
+        Scanner Sc =new Scanner(System.in); // Create a Scanner object for user input
+
+        System.out.println("Enter first number::>>"); // Prompt the user to enter the first number
+        a = Sc.nextInt(); // Read the first number from the user
+
+        System.out.println("Enter second number::>>"); // Prompt the user to enter the second number
+        b = Sc.nextInt(); // Read the second number from the user
+        System.out.println(" Enter your choice 1->ADD 2->SUBTRACT 3->MULTIPLY 4->DIVIDE \n Enter your choice::");
+        ch=Sc.nextInt();
+        if(ch==1){
+            System.out.println("Addition="+(a+b));
+        }
+        else if(ch==2){
+            System.out.println("Subtraction="+(a-b));
+        }
+        else if(ch==3){
+            System.out.println("Multiplacation="+(a*b));
+        }
+        else if(ch==4){
+            System.out.println("Division="+(a/b));
+        }
+        else{
+            System.out.println("Invalid choice");
+        }
+    }
+}
