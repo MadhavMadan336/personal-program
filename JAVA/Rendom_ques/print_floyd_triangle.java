c25e47939e1cb56fe586e8a1316781b8ebdb8618
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-10-19 19:27:06 +0530

    Create print_floyd_triangle.java

---
+++ JAVA/Rendom_ques/print_floyd_triangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class print_floyd_triangle {
+   public static void main(String args[]){
+      int n,i,j,k = 1;
+      System.out.println("Enter the number of lines you need in the FloyidsTriangle");
+      Scanner sc = new Scanner(System.in);
+      n = sc.nextInt();
+
+      for(i = 1; i <= n; i++) {
+         for(j=1;j <= i; j++){
+            System.out.print(" "+k++);
+         }
+         System.out.println();
+      }
+   }
+}
