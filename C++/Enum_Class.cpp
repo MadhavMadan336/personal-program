47fcb661916fab99d540d911771bb5ce9f3b830b
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-07-02 07:27:38 +0530

    Create Enum_Class.cpp

---
+++ C++/Enum_Class.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+enum class Color { Red, Green, Blue };
+int main() {
+    Color c = Color::Green;
+    if (c == Color::Green) {
+        std::cout << "Color is Green" << std::endl;
+    }
+    return 0;
+}
