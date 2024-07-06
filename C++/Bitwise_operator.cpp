671d3f0793cdbe6712ec1cbcb5e3a09b4514595c
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-07-06 10:35:15 +0530

    Create Bitwise_operator.cpp

---
+++ C++/Bitwise_operator.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+    int a = 5, b = 9;
+    std::cout << "a & b: " << (a & b) << std::endl;
+    std::cout << "a | b: " << (a | b) << std::endl;
+    std::cout << "a ^ b: " << (a ^ b) << std::endl;
+    return 0;
+}
