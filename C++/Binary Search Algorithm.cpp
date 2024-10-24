bf1099780e24321f2fb0ba37796db2e059d93630
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-10-24 14:12:20 +0530

    Create Binary Search Algorithm.cpp

---
+++ C++/Binary Search Algorithm.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int x) {
+    int left = 0, right = n - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == x) return mid;
+        if (arr[mid] < x) left = mid + 1;
+        else right = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 3, 4, 10, 40};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 10;
+    int result = binarySearch(arr, n, x);
+    (result == -1) ? cout << "Element not found" : cout << "Element found at index " << result;
+    return 0;
+}
