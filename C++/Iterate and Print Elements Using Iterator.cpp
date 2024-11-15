d2ffbc5eb4165ea3bbdeec7aa92e909c0ef8d6ce
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-11-15 12:51:59 +0530

    Create Iterate and Print Elements Using Iterator.cpp

---
+++ C++/Iterate and Print Elements Using Iterator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> nums = {1, 2, 3, 4, 5};
+    vector<int>::iterator it;
+    for (it = nums.begin(); it != nums.end(); ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
+    return 0;
+}
