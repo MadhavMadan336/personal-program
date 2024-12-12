fbde8bd0bb9bf7d3d80d226f53ad05f92160f06c
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-12-12 21:34:04 +0530

    Create countFrequency.cpp

---
+++ C++/countFrequency.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+void countFrequency(string str) {
+    unordered_map<char, int> freq;
+    for (char c : str) {
+        freq[c]++;
+    }
+
+    for (auto &pair : freq) {
+        cout << pair.first << ": " << pair.second << endl;
+    }
+}
+
+int main() {
+    string str = "hello";
+    countFrequency(str);
+    return 0;
+}
