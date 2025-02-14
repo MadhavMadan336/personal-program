17f9a71a0f3e5258574c4f8d4fa20dd65f98fd59
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2025-02-14 19:06:18 +0530

    Delete C++/Bitwise_operator.cpp

--- C++/Bitwise_operator.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main() {
-    int a = 5, b = 9;
-    std::cout << "a & b: " << (a & b) << std::endl;
-    std::cout << "a | b: " << (a | b) << std::endl;
-    std::cout << "a ^ b: " << (a ^ b) << std::endl;
-    return 0;
-}
