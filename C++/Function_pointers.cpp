a30c28113af4ba766fa849457223efdbe6f6b823
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2025-05-20 23:20:42 +0530

    Delete C++/Function_pointers.cpp

--- C++/Function_pointers.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int add(int a, int b) {
-    return a + b;
-}
-int main() {
-    int (*funcPtr)(int, int) = &add;
-    std::cout << "Sum: " << funcPtr(2, 3) << std::endl;
-    return 0;
-}
