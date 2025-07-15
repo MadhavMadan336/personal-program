7cce1902c489ecdf0fe1b09e5b92dcdd49ebee7b
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2025-07-15 08:09:07 +0530

    Delete C++/Template.cpp

--- C++/Template.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-template <typename T>
-T max(T a, T b) {
-    return (a > b) ? a : b;
-}
-int main() {
-    std::cout << "Maximum of 5 and 10: " << max(5, 10) << std::endl;
-    return 0;
-}
