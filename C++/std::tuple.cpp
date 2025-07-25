85ea1603217c8cce147412de201f98c8dd4ae37b
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2025-07-25 10:41:48 +0530

    Delete C++/std::tuple.cpp

--- C++/std::tuple.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <tuple>
-int main() {
-    std::tuple<int, std::string, double> t = {1, "Tuple", 3.14};
-    std::cout << "Tuple: " << std::get<0>(t) << ", " << std::get<1>(t) << ", " << std::get<2>(t) << std::endl;
-    return 0;
-}
