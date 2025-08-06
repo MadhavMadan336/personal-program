6a7ab8f6ef194bca8b02ec2734bbd7bfb1e9279a
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2025-08-06 15:27:19 +0530

    Delete C++/Std:shared_ptr.cpp

--- C++/Std:shared_ptr.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <memory>
-int main() {
-    std::shared_ptr<int> ptr1 = std::make_shared<int>(5);
-    std::shared_ptr<int> ptr2 = ptr1;
-    std::cout << "Value: " << *ptr1 << std::endl;
-    std::cout << "Shared count: " << ptr1.use_count() << std::endl;
-    return 0;
-}
