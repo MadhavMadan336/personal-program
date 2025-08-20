04b79c5e74fd785e111a1d998801b65d14323a26
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2025-08-20 13:34:59 +0530

    Delete C++/std::optional.cpp

--- C++/std::optional.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <optional>
-int main() {
-    std::optional<int> opt = 5;
-    if (opt) {
-        std::cout << "Optional has value: " << *opt << std::endl;
-    }
-    return 0;
-}
