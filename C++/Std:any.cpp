f12a22ee133ecc11dd5c074a9f0776ceb51c3009
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2025-08-22 17:40:58 +0530

    Delete C++/Std:any.cpp

--- C++/Std:any.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <any>
-int main() {
-    std::any a = 10;
-    std::cout << "Any as int: " << std::any_cast<int>(a) << std::endl;
-    a = "Hello";
-    std::cout << "Any as string: " << std::any_cast<const char*>(a) << std::endl;
-    return 0;
-}
