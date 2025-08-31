dddce49ff547261d7a63e5980ba2ae650e740168
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2025-08-31 17:40:08 +0530

    Delete C++/file_input_output.cpp

--- C++/file_input_output.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <fstream>
-int main() {
-    std::ofstream file("output.txt");
-    file << "Writing to a file" << std::endl;
-    file.close();
-    std::cout << "Data written to file." << std::endl;
-    return 0;
-}
