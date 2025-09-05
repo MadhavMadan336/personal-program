be1d3447e3c25d9b5fcf071651891f5feee66325
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2025-09-05 17:09:32 +0530

    Delete C++/exception_handling.cpp

--- C++/exception_handling.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main() {
-    try {
-        throw "Exception occurred!";
-    } catch (const char* msg) {
-        std::cerr << "Error: " << msg << std::endl;
-    }
-    return 0;
-}
