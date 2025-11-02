541c04f0cf395fb296288b221b501b2ab7508a97
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2025-11-02 09:25:08 +0530

    Add example of using vector from STL

---
+++ C++/Using vector from STL.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> v = {10, 20, 30};
+    v.push_back(40);
+    cout << "Vector elements: ";
+    for (int i : v)
+        cout << i << " ";
+}
