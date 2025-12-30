f588cce599fcf3ef68067b510e101903613961c9
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2025-12-30 08:57:44 +0530

    Implement SFINAE example to check for 'begin' method

---
+++ C++/SFINAE Example.cpp
@@ -0,0 +1,8 @@
+template<typename T>
+auto test(int) -> decltype(std::declval<T>().begin(), std::true_type{});
+
+template<typename>
+std::false_type test(...);
+
+template<typename T>
+using has_begin = decltype(test<T>(0));
