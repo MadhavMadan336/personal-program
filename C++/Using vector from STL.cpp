c5cf6bbf147e0678645b31d6eb260e780d5abfa4
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2026-01-02 09:35:05 +0530

    Fix output formatting for vector elements
    Updated the output format for vector elements.

--- C++/Using vector from STL.cpp
+++ C++/Using vector from STL.cpp
@@ -7,5 +7,5 @@ int main() {
     v.push_back(40);
     cout << "Vector elements: ";//print message bdhdhx
     for (int i : v)
-        cout << i << " ";
+        cout << i << " ";//what is the ndsjsh
 }
