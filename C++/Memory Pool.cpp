05a9f9f12b03fc529c15537472c556269d87987a
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2026-01-08 13:27:05 +0530

    Implement memory pool class for dynamic allocation

---
+++ C++/Memory Pool.cpp
@@ -0,0 +1,11 @@
+class Pool {
+    char* buffer;
+    size_t offset = 0;
+public:
+    Pool(size_t n) : buffer(new char[n]) {}
+    void* alloc(size_t n){
+        void* p = buffer + offset;
+        offset += n;
+        return p;
+    }
+};
