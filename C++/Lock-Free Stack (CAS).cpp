e44d6f37d204ff32b0374465bfb6f6a1527a97d3
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2026-01-10 13:54:41 +0530

    Implement Lock-Free Stack using CAS

---
+++ C++/Lock-Free Stack (CAS).cpp
@@ -0,0 +1,12 @@
+#include <atomic>
+
+template<typename T>
+class LockFreeStack {
+    struct Node { T data; Node* next; };
+    std::atomic<Node*> head{nullptr};
+public:
+    void push(T v){
+        Node* n = new Node{v, head.load()};
+        while(!head.compare_exchange_weak(n->next, n));
+    }
+};
