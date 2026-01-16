1dffa1f4cb968111507369837ac8a56f3a2d03c3
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2026-01-16 12:02:33 +0530

    Create Flyweight Pattern.cpp

---
+++ C++/Flyweight Pattern.cpp
@@ -0,0 +1,7 @@
+class Font {
+public:
+    static Font& get(const std::string& name){
+        static std::unordered_map<std::string, Font> fonts;
+        return fonts.try_emplace(name, Font()).first->second;
+    }
+};
