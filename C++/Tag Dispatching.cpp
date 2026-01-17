86062eabf18d51c307b11a45771b328e1079af31
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2026-01-17 21:04:40 +0530

    Delete C++/Tag Dispatching.cpp

--- C++/Tag Dispatching.cpp
+++
@@ -1,7 +0,0 @@
-void draw(int, std::true_type){ std::cout << "int\n"; }
-void draw(int, std::false_type){ std::cout << "other\n"; }
-
-template<typename T>
-void draw(T t){
-    draw(t, std::is_integral<T>{});
-}
