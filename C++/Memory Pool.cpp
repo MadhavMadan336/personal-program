8072b12dd23fe31b79bc9ab79babb3218bb82fb7
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2026-01-27 10:47:20 +0530

    Update Memory Pool.cpp

--- C++/Memory Pool.cpp
+++ C++/Memory Pool.cpp
@@ -1,6 +1,6 @@
 class Pool {
     char* buffer;
-    size_t offset = 0;
+    size_t offset = 0;//hejsjdjsuwndjdjednjd
 public:
     Pool(size_t n) : buffer(new char[n]) {}
     void* alloc(size_t n){
