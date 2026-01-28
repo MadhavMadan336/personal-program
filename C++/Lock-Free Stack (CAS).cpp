d7faa836abaaf07945264d885cd5bf56a72b6f18
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2026-01-28 09:17:44 +0530

    Fix formatting of Node struct in LockFreeStack

--- C++/Lock-Free Stack (CAS).cpp
+++ C++/Lock-Free Stack (CAS).cpp
@@ -2,7 +2,7 @@
 
 template<typename T>
 class LockFreeStack {
-    struct Node { T data; Node* next; };
+    struct Node { T data; Node* next; };//54649644
     std::atomic<Node*> head{nullptr};
 public:
     void push(T v){
