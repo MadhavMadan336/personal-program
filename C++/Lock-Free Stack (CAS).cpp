a3be1e2082166fc74584cb1c02627b14bf5068f0
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2026-02-03 17:47:11 +0530

    Fix comment formatting in push method

--- C++/Lock-Free Stack (CAS).cpp
+++ C++/Lock-Free Stack (CAS).cpp
@@ -6,7 +6,7 @@ class LockFreeStack {
     std::atomic<Node*> head{nullptr};
 public:
     void push(T v){
-        Node* n = new Node{v, head.load()};
+        Node* n = new Node{v, head.load()};//5888
         while(!head.compare_exchange_weak(n->next, n));
     }
 };
