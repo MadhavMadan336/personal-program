d62abfb5db006e87fca47cab5ffba6b1a754efaf
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2026-02-16 15:45:10 +0530

    Update Using vector from STL.cpp

--- C++/Using vector from STL.cpp
+++ C++/Using vector from STL.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    vector<int> v = {10, 20, 30};
+    vector<int> v = {10, 20, 30};//5555
     v.push_back(40);
     cout << "Vector elements: ";//print message bdhdhxhih
     for (int i : v)
