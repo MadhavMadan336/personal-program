606b0b4c34b421d6416304780d8e538d73f06d59
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2026-02-21 12:02:09 +0530

    Fix typo in Font class method

--- C++/Flyweight Pattern.cpp
+++ C++/Flyweight Pattern.cpp
@@ -2,6 +2,6 @@ class Font {
 public:
     static Font& get(const std::string& name){
         static std::unordered_map<std::string, Font> fonts;
-        return fonts.try_emplace(name, Font()).first->second;//2445ydye
+        return fonts.try_emplace(name, Font()).first->second;//2445ydyeyyy
     }
 };
