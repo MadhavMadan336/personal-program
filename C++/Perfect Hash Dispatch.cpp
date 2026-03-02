d5bb274c07926353671628b075ac9ab2af9aa425
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2026-03-02 13:23:49 +0530

    Update comment in handle function

--- C++/Perfect Hash Dispatch.cpp
+++ C++/Perfect Hash Dispatch.cpp
@@ -1,4 +1,4 @@
 template<int N>
 struct Handler {
-    static void handle(){ std::cout << N << "\n"; }//467hsjsjshihhejej
+    static void handle(){ std::cout << N << "\n"; }//467hsjsjshihhejejtttt
 };
