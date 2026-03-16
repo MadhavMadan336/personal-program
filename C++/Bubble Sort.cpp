935e06f97d54a7f86c82e4cbe022e6e00148cf0d
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2026-03-16 10:29:55 +0530

    Update Bubble Sort.cpp

--- C++/Bubble Sort.cpp
+++ C++/Bubble Sort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++)//knfldkflnkfj
+    for (int i = 0; i < n - 1; i++)//knfldk
         for (int j = 0; j < n - i - 1; j++)
             if (arr[j] > arr[j + 1])
                 swap(arr[j], arr[j + 1]);
