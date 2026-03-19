57cc2a77e18c3eb4e28e54a3acd3a2a5ea89686b
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2026-03-19 10:50:14 +0530

    Update Lock-Free Stack (CAS).cpp

--- C++/Lock-Free Stack (CAS).cpp
+++ C++/Lock-Free Stack (CAS).cpp
@@ -6,7 +6,7 @@ class LockFreeStack {
     std::atomic<Node*> head{nullptr};
 public:
     void push(T v){
-        Node* n = new Node{v, head.load()};//588
+        Node* n = new Node{v, head.load()};//588haba
         while(!head.compare_exchange_weak(n->next, n));
     }
 };
