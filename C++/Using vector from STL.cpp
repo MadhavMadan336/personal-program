f6677852526d3ccf442a930747ca8f8c224dc44b
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2026-04-09 10:51:34 +0530

    Modify comment in vector initialization
    Updated comment in vector initialization.

--- C++/Using vector from STL.cpp
+++ C++/Using vector from STL.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    vector<int> v = {10, 20, 30};//5555
+    vector<int> v = {10, 20, 30};//5
     v.push_back(40);
     cout << "Vector elements: ";//print
     for (int i : v)
